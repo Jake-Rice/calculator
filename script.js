5669c4ab53286ffae36aa80b90a115047e183d5e
Jake Rice <jrice189@gmail.com>
2022-11-27 11:27:16 -0800

    Add generalized  math operation function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Basic Math Functions
 function add(num1,num2) {
   return num1+num2;
 }
@@ -9,4 +10,15 @@ function multiply(num1,num2) {
 }
 function divide(num1,num2) {
   return num1/num2;
+}
+
+// Generalized Math Function
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "add": return add(num1,num2);
+    case "subtract": return subtract(num1,num2);
+    case "multiply": return multiply(num1,num2);
+    case "divide": return divide(num1,num2);
+    default: return;
+  }
 }
