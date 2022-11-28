26e4791c4f3a114923b254e11856f581b36042de
Jake Rice <jrice189@gmail.com>
2022-11-27 16:13:37 -0800

    Add functionality to number and operator buttons

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let displayValue;
+let num1;
+let num2;
+let operation;
+init();
+
+function init() {
+  displayValue = "0";
+  num1 = null;
+  num2 = null;
+  operation = null;
+  const buttons = document.querySelectorAll("button");
+  const numKeys = [];
+  const operKeys = [];
+  buttons.forEach(e => {
+    if (/btn-num-[0-9]/.test(e.id)) numKeys.push(e);
+    else if (/btn-oper-/.test(e.id)) operKeys.push(e);
+  });
+  numKeys.forEach(e => e.addEventListener("click", event => {
+    displayValue += e.id.slice(8);
+    //trim leading zeros
+    while (displayValue.length>1 && displayValue[0]==="0") displayValue = displayValue.slice(1);
+    document.querySelector("#display").textContent = displayValue;
+  }));
+  operKeys.forEach((e) => e.addEventListener("click", event => {
+    if (!operation) {
+      num1 = +displayValue;
+    }
+    else {
+      num2 = displayValue;
+      num1 = operate(operation, +num1, +num2);
+      displayValue = num1;
+      document.querySelector("#display").textContent = displayValue;
+    }
+    operation = e.id.slice(9);
+    displayValue = 0;
+    console.log(num1, operation);
+  }));
+}
+
 // Basic Math Functions
 function add(num1,num2) {
   return num1+num2;
